44dec1a71900b3d013e08aeb26f481034edf338f
Chase Westover <chasejwestover@gmail.com>
2021-02-22 19:04:20 -0600

    started the exercise

--- jquery.js
+++ jquery.js
@@ -1,3 +1,9 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('Ready for JQuery!');
 });
+
+$("article img").attr("class","image-center");
+$("p").last().remove();
+$("#title").css("font-size", `${Math.floor((Math.random()*100))}px`)
+$("ol").append( $("<li>").text("new list value") );
+$("aside").empty().append( $("<p>").text("So sorry for the stupid aside"));
